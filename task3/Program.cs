492519b9898c472e5d9c8b445f19a106722e485d
Dmitriy B <kupperfeet@gmail.com>
2022-08-15 12:15:14 +0300

    Task 3 ArrayLength  done

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 42, 32, 45, 51, 6, 7, 32, 9};
+
+int n = array.Length;
+int find = 32;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
